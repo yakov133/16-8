0f9dc057902cf0f0e71964547679d0ed3509eb37
“yakov133” <yakov133@walla.com>
2021-08-17 13:02:07 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,109 @@
+// let firstName = prompt("insert your name:");
+// let lastName = prompt("insert your last name:")
+
+
+//targil 1
+// if(firstName.length > lastName.length){
+//     print(firstName);
+// }else{
+//     print(lastName);
+// }
+
+// function print(myString){
+//     console.log(myString);
+// }
+
+
+
+//targil 2
+// lookingInString(firstName,"a");
+// lookingInString(lastName,"api");
+// function lookingInString( myString , serchFor){    
+//     return (myString.indexOf(serchFor) != -1);
+// }
+
+
+
+//targil 3
+// let userInput = prompt("give me your full name")
+// console.log(`ther is ${userInput.length} of characters`);
+// lookingInString(userInput," ");
+// if(lookingInString(userInput," ")){
+//     let index = userInput.indexOf(" ");
+//     console.log(`the first name is: ${userInput.substr(0,index)}`);
+//     console.log(`the last name is: ${userInput.substr(index + 1 )}`);
+// }
+
+
+
+//targil 4 
+// function lengthAboveFive(mystring, len){
+//     if(mystring.length > len){
+//         return "long";
+//     }
+//     return "short"
+// };
+// console.log(lengthAboveFive("yakov", 5));
+
+
+
+//targil 5
+// function lengthAboveThree(mystring, len){
+//     if(mystring.length > len){
+//         return "yes";
+//     }
+//     return "no"
+// };
+// console.log(lengthAboveThree("yakov", 3));
+
+
+//targil 6
+// function targil6(myString,tav){
+//     if(myString.length > 6){
+//         return myString;
+//     }else{
+//         return myString.indexOf(tav)
+//     }
+// }
+// console.log( targil6("yakov",'a') );
+
+
+
+//targil 7
+// function targil7(myString,tav){
+//     if(myString.indexOf(tav) != -1){
+//         return myString.indexOf(tav);
+//     }else{
+//         return myString;
+//     }
+// }
+// console.log( targil7("yakov",'a') );
+
+
+//targil 8
+// function targil8(firstName, lastName){
+//     return firstName[0] + "." + " "+ lastName.toUpperCase();
+// }
+// console.log(targil8(firstName,lastName));
+
+
+//targil 9
+// function targil9(name1, name2){
+//     if(name1.length > name2.length){
+//         console.log(name2.toUpperCase());
+//         return name1
+//     }
+//     console.log(name1.toUpperCase());
+//     return name2;
+// }
+// console.log(targil9("yakov","kassa"));
+
+
+//targil 10
+// function targil10(myString, tav){
+//     if(myString.indexOf(tav) != -1){
+//         return myString;
+//     }
+//     alert(`The tav '${tav}' not Exists in '${myString}'`);
+// }
+// console.log(targil10("yakov","ae"));
